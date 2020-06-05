b0ac6c872ff898b64c8e73fe8acb54d9752bfee7
xuwen <xuwen@126.com>
2020-06-05 13:37:37 +0800

    init pinet tensorrt project

---
+++ build/CMakeFiles/3.10.2/CMakeSystem.cmake
@@ -0,0 +1,21 @@
+set(CMAKE_HOST_SYSTEM "Linux-sh: 0: getcwd() failed: No such file or directory
+4.14.150-tegra")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "sh: 0: getcwd() failed: No such file or directory
+4.14.150-tegra")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "sh: 0: getcwd() failed: No such file or directory
+aarch64")
+
+
+
+set(CMAKE_SYSTEM "Linux-sh: 0: getcwd() failed: No such file or directory
+4.14.150-tegra")
+set(CMAKE_SYSTEM_NAME "Linux")
+set(CMAKE_SYSTEM_VERSION "sh: 0: getcwd() failed: No such file or directory
+4.14.150-tegra")
+set(CMAKE_SYSTEM_PROCESSOR "sh: 0: getcwd() failed: No such file or directory
+aarch64")
+
+set(CMAKE_CROSSCOMPILING "FALSE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ build/CMakeFiles/PINetTensorrt.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tangcheng/xuwen/PINetTensorrt/PINetTensorrt.cpp" "/home/tangcheng/xuwen/PINetTensorrt/build/CMakeFiles/PINetTensorrt.dir/PINetTensorrt.cpp.o"
+  "/home/tangcheng/xuwen/PINetTensorrt/common/getOptions.cpp" "/home/tangcheng/xuwen/PINetTensorrt/build/CMakeFiles/PINetTensorrt.dir/common/getOptions.cpp.o"
+  "/home/tangcheng/xuwen/PINetTensorrt/common/logger.cpp" "/home/tangcheng/xuwen/PINetTensorrt/build/CMakeFiles/PINetTensorrt.dir/common/logger.cpp.o"
+  "/home/tangcheng/xuwen/PINetTensorrt/common/sampleEngines.cpp" "/home/tangcheng/xuwen/PINetTensorrt/build/CMakeFiles/PINetTensorrt.dir/common/sampleEngines.cpp.o"
+  "/home/tangcheng/xuwen/PINetTensorrt/common/sampleOptions.cpp" "/home/tangcheng/xuwen/PINetTensorrt/build/CMakeFiles/PINetTensorrt.dir/common/sampleOptions.cpp.o"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../common"
+  "/usr/local/cuda/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
